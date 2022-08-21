35291e6f41bab0bab40c99cf67bc58d5520fc57c
crono628 <84817352+crono628@users.noreply.github.com>
2022-08-21 18:24:02 -0400

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Slider from '@mui/material/Slider';
 
 function App() {
   const [water, setWater] = useState(250);
@@ -13,48 +14,48 @@ function App() {
     setBeans(water / e.target.value);
   };
   return (
-    <div>
+    <div className="app-container">
       <div className="big-c flex">
         <div>C</div>
         <div>
-          <div className="little-letters">offee</div>
-          <div className="little-letters">alculations</div>
+          <div className="small-letters">offee</div>
+          <div className="small-letters">alculations</div>
         </div>
       </div>
-      <div className="little-letters flex column">
-        <span className="flex">
+      <div className="small-letters">
+        <div>
           <label htmlFor="ratio">Ratio: {ratio}</label>
-          <input
-            className="little-letters"
-            style={{ width: '65px', marginLeft: '5px', paddingLeft: '10px' }}
-            type="range"
-            name="ratio"
-            id=""
-            min="10"
-            max="20"
-            onChange={handleRatio}
+          <Slider
             value={ratio}
+            onChange={handleRatio}
+            step={1}
+            marks
+            min={10}
+            max={20}
+            size="large"
           />
-        </span>
+        </div>
         <div style={{ fontSize: '12px', marginLeft: '5px' }}>
-          {' '}
           (lower number is stronger)
         </div>
-        <div style={{ marginTop: '2rem' }}>Water: {water}</div>
+        <div className="relative" style={{ marginTop: '2rem' }}>
+          <span className="absolute width-100">Water: {water} ml</span>
+        </div>
       </div>
-      <div className="flex column little-letters">
-        <input
-          style={{ width: '200px', marginLeft: '5px', paddingLeft: '10px' }}
-          step={10}
-          type="range"
-          name=""
-          onChange={handleChange}
+      <div className="small-letters margin-top-30 width-80">
+        <Slider
           value={water}
-          min="250"
-          max="1250"
+          onChange={handleChange}
+          step={25}
+          marks
+          min={250}
+          max={1250}
+          size="large"
         />
-        <div>Beans: {Math.round(beans)}</div>
-      </div>{' '}
+      </div>
+      <div className="relative" style={{ fontSize: '1.5rem' }}>
+        <span className=" width-100">Beans: {Math.round(beans)} grams</span>
+      </div>
     </div>
   );
 }
